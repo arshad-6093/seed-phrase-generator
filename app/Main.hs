e73f43bbc6e8c0411e40f559cbecbe0a2fa73108
arshad-6093 <arshad.kaleelrahman@gmail.com>
2024-06-07 16:54:44 +0530

    Making some changes

--- app/Main.hs
+++ app/Main.hs
@@ -1,57 +1,9 @@
-import           Control.Monad (replicateM)
-import qualified Crypto.Hash as Crypto
-import           Data.ByteString ()
-import qualified Data.ByteString.Char8 as BS
-import           Data.ByteArray (convert)
-import           Data.Bits (shiftR)
-import           Data.Byteable ()
-import           Data.Char (digitToInt, intToDigit)
-import           Data.List (foldl', unfoldr)
-import           System.Random (randomRIO)
-import           Numeric (showIntAtBase)
+module Main where
 
--- Load the BIP-39 wordlist from a file
-loadWordlist :: FilePath -> IO [String]
-loadWordlist path = do
-    content <- readFile path
-    return $ lines content
-
--- Generate a random seed phrase of 12 words
-generateSeedPhrase :: [String] -> IO [String]
-generateSeedPhrase wordlist = do
-    -- Generate 128 bits of entropy (16 bytes)
-    entropy <- replicateM 16 (randomRIO (0, 255) :: IO Int)
-    let entropyBytes = BS.pack $ map (toEnum . fromEnum) entropy
-
-    -- Hash the entropy with SHA-256 to get the checksum
-    let hash = Crypto.hashWith Crypto.SHA256 entropyBytes
-        hashBytes = convert hash  -- Convert digest to ByteString
-        checksumBits = take 1 (BS.unpack hashBytes)  -- 4 bits for 128 bits of entropy
-        checksum = fromEnum (head checksumBits) `shiftR` 4  -- Extract the first 4 bits
-
-    -- Convert entropy + checksum to a binary string
-    let binaryString = concatMap (padLeft 8 '0' . flip showBinary 2) entropy
-        binaryWithChecksum = binaryString ++ padLeft 4 '0' (showBinary checksum 2)
-
-    -- Split the binary string into 11-bit chunks and map to words
-    let wordIndices = unfoldr (\b -> if null b then Nothing else Just $ splitAt 11 b) binaryWithChecksum
-        wordIndicesInt = map binaryToInt wordIndices
-    return $ map (wordlist !!) wordIndicesInt
-
--- Helper functions
-padLeft :: Int -> a -> [a] -> [a]
-padLeft n c xs = replicate (n - length xs) c ++ xs
-
-showBinary :: Int -> Int -> String
-showBinary x n = padLeft n '0' $ showIntAtBase 2 intToDigit x ""
-
-binaryToInt :: String -> Int
-binaryToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+import SeedPhrase (generateSeedPhrase)
 
 main :: IO ()
 main = do
-    -- Load the full BIP-39 wordlist
-    wordlist <- loadWordlist "app/bip39.txt"
-    seedPhrase <- generateSeedPhrase wordlist
-    putStrLn "Generated Seed Phrase:"
-    putStrLn $ unwords seedPhrase
+  seedPhrase <- generateSeedPhrase
+  putStrLn "Generated Seed Phrase:"
+  putStrLn $ unwords seedPhrase
